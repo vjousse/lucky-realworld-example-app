78af0e5c3748b3454b897c549d5c675223a25ed3
Vincent Jousse <vincent@jousse.org>
2020-09-27 21:17:07 +0100

    Avoid Avram error

--- src/actions/api/profiles/follow.cr
+++ src/actions/api/profiles/follow.cr
@@ -3,7 +3,7 @@ class Api::Profiles::Follow < ApiAction
   post "/api/profiles/:username/follow" do
     user = UserQuery.new.username(username).first
 
-    already_following = FollowQuery.new.follower_id(current_user.id).following_id(user.id).first
+    already_following = FollowQuery.new.follower_id(current_user.id).following_id(user.id).first?
 
     FollowProfile.create!(follower_id: current_user.id, following_id: user.id) unless already_following
 
