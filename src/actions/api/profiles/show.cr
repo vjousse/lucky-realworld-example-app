a0ddba882f5f5b8685f33df578b76f96441839d1
Vincent Jousse <vincent@jousse.org>
2020-09-28 20:22:38 +0100

    Refactor is_following in it's own method

--- src/actions/api/profiles/show.cr
+++ src/actions/api/profiles/show.cr
@@ -5,12 +5,9 @@ class Api::Profiles::Show < ApiAction
   get "/api/profiles/:username" do
     user = UserQuery.new.username(username).first
 
-    if logged_in_user = current_user
-      follow = FollowQuery.new.follower_id(logged_in_user.id).following_id(user.id).any?
-    else
-      follow = false
-    end
-
-    json ProfileSerializer.new(user, follow)
+    json ProfileSerializer.new(
+      user,
+      FollowQuery.new.is_following(current_user, user)
+    )
   end
 end
