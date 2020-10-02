b2990555546bd167c23ae227b0f9b1108574843a
Vincent Jousse <vincent@jousse.org>
2020-10-02 13:37:32 +0100

    Move article tagging into its own query method

--- src/actions/api/articles/create.cr
+++ src/actions/api/articles/create.cr
@@ -3,24 +3,10 @@ class Api::Articles::Create < ApiAction
 
     article = SaveArticle.create!(params, author_id: current_user.id)
 
-    if tags = params.from_json["article"]["tagList"].as_a
-      tags.map(&.as_s).each do |name|
-
-        SaveTag.create(name: name) do |operation, created_tag|
-          if created_tag
-            tagging = SaveTagging.create!(article_id: article.id, tag_id: created_tag.id)
-          else
-            new_tag = TagQuery.new.name(name).first
-
-            if !article.reload(&.preload_tags).tags.includes?(new_tag)
-              tagging = SaveTagging.create!(article_id: article.id, tag_id: new_tag.id)
-            end
-
-          end
-        end
-
-      end
-    end
+    ArticleQuery.new.add_tags(
+      article,
+      params.from_json["article"]["tagList"].as_a.map(&.as_s)
+    )
 
     json ArticleSerializer.new(article.reload(&.preload_tags))
   end
