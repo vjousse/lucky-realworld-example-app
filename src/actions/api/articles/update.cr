30015adcd271a0001f8703a18d4b55e393ee81dd
Vincent Jousse <vincent@jousse.org>
2020-10-02 13:48:11 +0100

    Add article update

---
+++ src/actions/api/articles/update.cr
@@ -0,0 +1,16 @@
+class Api::Articles::Update < ApiAction
+  put "/api/articles/:slug" do
+
+    article = ArticleQuery.new.slug(slug).first
+    SaveArticle.update(article, params) do |operation, updated_article|
+
+      ArticleQuery.new.add_tags(
+        updated_article,
+        params.from_json["article"]["tagList"].as_a.map(&.as_s)
+      )
+    end
+
+    json ArticleSerializer.new(article.reload(&.preload_tags))
+
+  end
+end
