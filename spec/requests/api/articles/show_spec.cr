693826ddca9f29bf77883729fffa5b69b1a6425b
Vincent Jousse <vincent@jousse.org>
2020-10-06 07:02:26 +0100

    Complete article show test

--- spec/requests/api/articles/show_spec.cr
+++ spec/requests/api/articles/show_spec.cr
@@ -1,7 +1,7 @@
 require "../../../spec_helper"
 
 describe Api::Articles::Show do
-  it "401 if not authenticated" do
+  it "returns 401 if not authenticated" do
 
     article = ArticleBox.create
     response = ApiClient.exec(Api::Articles::Show.with(article.slug))
@@ -10,11 +10,24 @@ describe Api::Articles::Show do
   end
 
 
-  it "ok if authenticated" do
+  it "is ok if authenticated" do
     article = ArticleBox.create.reload(&.preload_author)
 
     response = ApiClient.auth(article.author).exec(Api::Articles::Show.with(article.slug))
 
+
+    json_response = JSON.parse(response.body)
+
     response.status_code.should eq(200)
+    json_response["article"]["title"].as_s.should eq(article.title)
+  end
+
+
+  it "returns 404 if not found" do
+    user = UserBox.create
+
+    response = ApiClient.auth(user).exec(Api::Articles::Show.with("unknow"))
+
+    response.status_code.should eq(404)
   end
 end
