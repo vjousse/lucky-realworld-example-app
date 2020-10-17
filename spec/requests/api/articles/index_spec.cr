afcb55bb3a3ee737444d79b1d7225d3eed477759
Vincent Jousse <vincent@jousse.org>
2020-10-17 12:03:50 +0100

    Add limit test

--- spec/requests/api/articles/index_spec.cr
+++ spec/requests/api/articles/index_spec.cr
@@ -14,5 +14,20 @@ describe Api::Articles::Index do
     response.status_code.should eq(200)
   end
 
+
+  it "filters with limit" do
+    limit = 2
+    article1 = ArticleBox.create.reload(&.preload_author)
+    article2 = ArticleBox.create.reload(&.preload_author)
+    article3 = ArticleBox.create.reload(&.preload_author)
+
+    response = ApiClient.auth(article1.author).exec(Api::Articles::Index.with(limit: limit))
+
+    json_response = JSON.parse(response.body)
+
+    json_response["articlesCount"].as_i.should eq(limit)
+    response.status_code.should eq(200)
+  end
+
 end
 
