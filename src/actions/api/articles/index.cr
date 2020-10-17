e199b7fdc9f833769752a119a610a977343d3b96
Vincent Jousse <vincent@jousse.org>
2020-10-17 11:22:04 +0100

    Add tag filter

--- src/actions/api/articles/index.cr
+++ src/actions/api/articles/index.cr
@@ -1,9 +1,19 @@
 class Api::Articles::Index < ApiAction
-
   include Api::Auth::SkipRequireAuthToken
 
+  param tag : String?
+  param author : String?
+  param favorited : String?
+  param limit : Int?
+  param offset : Int?
+
   get "/api/articles" do
-    articles = ArticleQuery.new.preload_tags.preload_author
-    json ArticleSerializer.for_collection_with_key(articles, "articlesCount")
+    articles_query = ArticleQuery.new.preload_tags.preload_author
+
+    if tag_filter = tag
+      articles_query = articles_query.where_tags(TagQuery.new.name(tag_filter))
+    end
+
+    json ArticleSerializer.for_collection_with_key(articles_query, "articlesCount")
   end
 end
